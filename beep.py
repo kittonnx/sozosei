930871779d4adf9f8634bbdec41b143ee709973e
otani toshiki <sd22022@toyota-ti.ac.jp>
2024-10-25 13:30:00 +0900

    beeptest

---
+++ beep.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+#Set buzzer - pin 23 as output
+buzzer=23
+GPIO.setup(buzzer,GPIO.OUT)
+
+
+GPIO.output(buzzer,GPIO.HIGH)
+print ("Beep")
+sleep(0.5) # Delay in seconds
+GPIO.output(buzzer,GPIO.LOW)
+print ("No Beep")
+sleep(0.5)
+    
